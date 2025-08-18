324d4671979c262225d376adaaba566b0518254f
GradientAscent <scaledgradient@gmail.com>
2025-08-17 23:03:39 -0600

    update to version 1.21.7
    Signed-off-by: GradientAscent <scaledgradient@gmail.com>

--- src/main/java/gradientascent/bookbook/data/provider/BookBookBlockTagProvider.java
+++ src/main/java/gradientascent/bookbook/data/provider/BookBookBlockTagProvider.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -16,8 +15,6 @@ public class BookBookBlockTagProvider extends FabricTagProvider.BlockTagProvider
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
-//        getTagBuilder(BlockTags.NEEDS_DIAMOND_TOOL).addTag(Identifier.of(BookBookBlocks.SUNDERING_TABLE.toString()));
-//        getTagBuilder(BlockTags.PICKAXE_MINEABLE).addTag(Identifier.of(BookBookBlocks.SUNDERING_TABLE.toString()));
         valueLookupBuilder(BlockTags.NEEDS_DIAMOND_TOOL).add(BookBookBlocks.SUNDERING_TABLE);
         valueLookupBuilder(BlockTags.PICKAXE_MINEABLE).add(BookBookBlocks.SUNDERING_TABLE);
     }
